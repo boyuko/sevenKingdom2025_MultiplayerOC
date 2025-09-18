b318b3ce63003626601ef5b660691a6ffddf6983
boyuko <s82300265@gmail.com>
2025-09-19 05:21:18 +0800

    add reset origin function for multiplayer add vertical mode for BP_CubeMirror update scene 1 and 2 add BP_LivelinkController for replication livelink objects (vrpn)

--- Source/MultiplayerOC/MultiplayerOC.Build.cs
+++ Source/MultiplayerOC/MultiplayerOC.Build.cs
@@ -10,7 +10,7 @@ public class MultiplayerOC : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] {  "RenderCore",  "RHI"    });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
